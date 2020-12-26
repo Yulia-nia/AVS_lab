955225b217cb081dc201d8a8043559e02981c797
Yulia-nia <sylianetttogggg@gmail.com>
2020-12-26 19:56:09 +0300

    lab 3

---
+++ Netetskaya_lab_3/Netetskaya_lab_3/Netetskaya_lab_3.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <omp.h>
+#include <vector>
+#include <chrono>
+#include <random>
+
+
+int vector_size = 1000000;
+
+// Вариант 17. Скалярное произведение двух векторов.
+
+void parallel(const std::vector<int>& vector_1, const std::vector<int>& vector_2)
+{
+	auto start_time = clock();
+	int result = 0;	
+#pragma omp parallel for shared(vector_1,vector_2,result) private(i) schedule(auto) reduction(+:result)
+	for (int i = 0; i < vector_size; i++)
+	{
+		result += vector_1[i] * vector_2[i];
+	}
+	auto time = clock() - start_time;
+	std::cout << "Parallel computation." << std::endl<< "Result:" << result << std::endl<< "Time: " << time << std::endl;
+}
+
+void simple(const std::vector<int>& vector_1, const std::vector<int>& vector_2)
+{
+	auto start_time = clock();
+
+	int result = 0;
+
+	for (int i = 0; i < vector_size; i++)
+	{
+		result += vector_1[i] * vector_2[i];
+	}
+	auto time = clock() - start_time;
+	std::cout << "One branch."<< result << std::endl<< "Result:" << result << std::endl<< "Time: " << time << std::endl;
+}
+
+
+
+int main()
+{
+	std::vector<int> vector_1(vector_size, 1), vector_2(vector_size, 1);
+
+	for (int i = 0; i < vector_size; i++)
+	{
+		vector_1[i] = rand() % 10;
+	}
+
+	for (int i = 0; i < vector_size; i++)
+	{
+		vector_2[i] = rand() % 10;
+	}
+
+	simple(vector_1, vector_2);
+	parallel(vector_1, vector_2);
+
+	return 0;
+}
