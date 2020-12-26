963ad1093790f774dc9efe86b5cd61755a999d0e
Yulia-nia <sylianetttogggg@gmail.com>
2020-12-26 21:20:05 +0300

    lab 1 update

--- Netetskaya_lab1/Netetskaya_lab1/Netetskaya_lab1.cpp
+++ Netetskaya_lab1/Netetskaya_lab1/Netetskaya_lab1.cpp
@@ -13,25 +13,26 @@ void variant_17(__int8* A, __int8* B, __int8* C, __int16* D)
 
 		mov			ESI, A
 		movq		mm2, [ESI]
-
-		movq		mm0, mm2
-		pcmpgtb		mm1, mm0
-		punpcklbw	mm0, mm1
-		movq		mm7, mm0		
-
-		movq		mm0, mm2
-		pcmpgtb		mm1, mm0
+		pxor  mm1, mm1
+		movq		mm0, mm2	
+		pcmpgtb		mm1, mm0	
+		punpcklbw	mm0, mm1	
+		movq		mm7, mm0	
+		pxor  mm1, mm1
+		movq		mm0, mm2	
+		pcmpgtb		mm1, mm0	
 		punpckhbw	mm0, mm1
 		movq		mm6, mm0	
 
+		
 		mov			ESI, B
 		movq		mm3, [ESI]
-
-		movq		mm0, mm3
+		pxor  mm1, mm1
+		movq		mm0, mm3 
 		pcmpgtb		mm1, mm0
 		punpcklbw	mm0, mm1
 		movq		mm5, mm0	
-
+		pxor  mm1, mm1
 		movq		mm0, mm3
 		pcmpgtb		mm1, mm0
 		punpckhbw	mm0, mm1
@@ -40,31 +41,34 @@ void variant_17(__int8* A, __int8* B, __int8* C, __int16* D)
 		psubsw		mm7, mm5
 		psubsw		mm6, mm4
 		
+
 		mov			ESI, C
 		movq		mm3, [ESI]
+		pxor  mm1, mm1
+		movq		mm0, mm3	
+		pcmpgtb		mm1, mm0	
+		punpcklbw	mm0, mm1	
+		movq		mm5, mm0	
+		pxor  mm1, mm1
+		movq		mm0, mm3	
+		pcmpgtb		mm1, mm0	
+		punpckhbw	mm0, mm1	
+		movq		mm4, mm0	
 
-		movq		mm0, mm3
-		pcmpgtb		mm1, mm0
-		punpcklbw	mm0, mm1
-		movq		mm5, mm0
-
-		movq		mm0, mm3
-		pcmpgtb		mm1, mm0
-		punpckhbw	mm0, mm1
-		movq		mm4, mm0
 
 		mov			ESI, D
 		movq		mm0, [ESI]
-		mov			ESI, D
-		movq		mm1, [ESI + 2 * 4]
 
-		psubsw		mm5, mm0
+		mov			ESI, D
+		movq		mm1, [ESI + 2 * 4]	
+
+		psubsw		mm5, mm0	
 		psubsw		mm4, mm1
 
-		paddw		mm5, mm7
+		paddw		mm5, mm7	
 		paddw		mm4, mm6
-
-		movq [F], mm5
+		
+		movq [F], mm5			
 		movq [F + 2 * 4], mm4
 	}	
 
@@ -91,6 +95,16 @@ int main()
 	__int16 D[8]{ 50, -300, 46, 500, -1000, 250, 2000, 10};
 	
 	variant_17(A, B, C, D);
+
+
+
+	__int8 A_2[8]{ -10, 5, 19, -4, -165, 120, 82, 1 };
+	__int8 B_2[8]{ 30, 94, 52, -78, 11, -32, 910, -110 };
+	__int8 C_2[8]{ -5, 70, 80, 12, -7, 14, -122, 3 };
+	__int16 D_2[8]{ 50, -3000, 16, 5000, -1000, 2500, 2000, 10 };
+
+	variant_17(A_2, B_2, C_2, D_2);
+
 	system("pause");
 	return 0;
 }
